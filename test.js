3b36db7b9b06a43a32c72023235d571eb748b172
Alvin <alvinzcoxiv@gmail.com>
2020-07-11 13:19:14 -0400

    makers not working on map, using wring coordinates to search for places

--- test.js
+++ test.js
@@ -1,23 +1,10 @@
 $(document).ready(function() {
 
-    // $("#findplace").on("click", function() {
-    //     var placeField = $("#placefield").val().trim()
-    //     var queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + placeField + "&inputtype=textquery&fields=photos,formatted_address,name,geometry&key=AIzaSyCFP0tqM6o9f2FoX3gInG3rF-5nGBZiKc8"
-
-    //     $.ajax({
-    //         url: queryURL,
-    //         method: "GET"
-    //     }).then(function(response) {
-    //         console.log(response)
-    //         console.log("Can you see this?")
-    //     }).fail(function(err) {
-    //         console.log(err)
-    //     })
-    // })
-
     var latitude
     var longitude
     var map;
+    var service
+    var infowindow
     function initMap() {
         map = new google.maps.Map(document.getElementById("map"), {
             center: {lat: latitude, lng: longitude},
@@ -39,13 +26,73 @@ $(document).ready(function() {
         initMap()
     }
 
+    function createMarker(markerLocation) {
+        var marker = new google.maps.Marker({
+            position: {lat: latitude, lng: longitude},
+            title:"Hello World!"
+        });
+       
+        marker.setMap(map);
+    }
+
+    function findDate() {
+        var distMiles = $("#searchradius").val()
+        var distMeters = distMiles * 1610
+        var cuisine = $("#cuisine").val()
+        var currentMapCoords = latitude + ", " + longitude
+        var searchLocation = new google.maps.LatLng(currentMapCoords)
+
+        console.log(distMiles)
+        console.log(distMeters)
+        console.log(cuisine)
+        console.log(currentMapCoords)
+        console.log(searchLocation)
+        
+        var request = {
+            location: searchLocation,
+            radius: distMeters.toString(),
+            type: ["restaruant"],
+            query: cuisine.toString()
+          };
+      
+          service = new google.maps.places.PlacesService(map);
+          service.textSearch(request, callback);
+      }
+      
+        function callback(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                var place = results[i];
+                console.log(place)
+                var placeDetailId = place.place_id
+
+                var requestDetail = {
+                    placeId: placeDetailId,
+                    fields: ['name', 'rating', 'formatted_phone_number', 'geometry', 'photo', 'url']
+                  };
+                  
+                  service = new google.maps.places.PlacesService(map);
+                  service.getDetails(requestDetail, callback);
+                  
+                  function callback(placeDetail, status) {
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        console.log(placeDetail)
+                    //   createMarker(placeDetail);
+                    }
+                  }
+            }
+        }
+    }
+
     
+
+
     getLocation()
 
     $("#centermap").on("click", function(event) {
         event.preventDefault()
         navigator.geolocation.getCurrentPosition(getPosition)
-        console.log("button is clicked")
+        // console.log("button is clicked")
     })
 
     $("#findcity").on("click", function(event) {
@@ -58,15 +105,24 @@ $(document).ready(function() {
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response)
-            console.log(response.results[0].geometry.location.lat)
-            console.log(response.results[0].geometry.location.lng)
+            // console.log(response)
+            // console.log(response.results[0].geometry.location.lat)
+            // console.log(response.results[0].geometry.location.lng)
 
             latitude = response.results[0].geometry.location.lat
             longitude = response.results[0].geometry.location.lng
+            console.log(latitude)
+            console.log(longitude)
 
             initMap()
         })
     })
+
+    $("#findrestaurant").on("click", function(event) {
+        event.preventDefault()
+        findDate()
+    })
+
+
 })
 
